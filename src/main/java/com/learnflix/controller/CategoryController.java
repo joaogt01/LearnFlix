431a406d58a0d29d18b4ea0f412278e4378d19d4
João Victor <joao.dev06@gmail.com>
2025-11-09 22:09:44 -0300

    feat: implementa a entidade Category

--- src/main/java/com/learnflix/controller/CategoryController.java
+++ src/main/java/com/learnflix/controller/CategoryController.java
@@ -1,8 +1,13 @@
 package com.learnflix.controller;
 
+import com.learnflix.controller.request.CategoryRequest;
+import com.learnflix.controller.response.CategoryResponse;
 import com.learnflix.entity.Category;
+import com.learnflix.mapper.CategoryMapper;
 import com.learnflix.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,26 +21,30 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping()
-    public List<Category> getAllCategories(){
-        return categoryService.findAll();
+    public List<CategoryResponse> getAllCategories(){
+        List<Category> categories = categoryService.findAll();
+        return categories.stream()
+                .map(CategoryMapper::toCategoryResponse)
+                .toList();
     }
 
     @GetMapping("/{id}")
-    public Category getByCategoryId(@PathVariable Long id){
-        Optional<Category> optionalCategory = categoryService.findById(id);
-        if (optionalCategory.isPresent()){
-            return optionalCategory.get();
-        }
-        return null;
+    public ResponseEntity<CategoryResponse> getByCategoryId(@PathVariable Long id){
+        return categoryService.findById(id)
+                .map(category -> ResponseEntity.ok(CategoryMapper.toCategoryResponse(category)))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public Category saveCategory(@RequestBody Category category){
-        return categoryService.saveCategory(category);
+    public CategoryResponse saveCategory(@RequestBody CategoryRequest request){
+        Category newCategory = CategoryMapper.toCategory(request);
+        Category savedCategory = categoryService.saveCategory(newCategory);
+        return CategoryMapper.toCategoryResponse(savedCategory);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCategoryById(@PathVariable Long id){
+    public ResponseEntity<Void> deleteCategoryById(@PathVariable Long id){
         categoryService.deleteCategory(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
---
+++ src/main/java/com/learnflix/mapper/CategoryMapper.java
@@ -0,0 +1,26 @@
+package com.learnflix.mapper;
+
+import com.learnflix.controller.request.CategoryRequest;
+import com.learnflix.controller.response.CategoryResponse;
+import com.learnflix.entity.Category;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class CategoryMapper {
+
+    public static Category toCategory(CategoryRequest categoryRequest){
+        return Category
+                .builder()
+                .name(categoryRequest.name())
+                .build();
+    }
+
+    public static CategoryResponse toCategoryResponse(Category category){
+        return CategoryResponse
+                .builder()
+                .id(category.getId())
+                .name(category.getName())
+                .build();
+    }
+
+}
