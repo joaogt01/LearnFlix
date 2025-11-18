cc40de88af2e06a3ff63543d08ba6c8eec967230
João Victor <joao.dev06@gmail.com>
2025-11-17 23:23:06 -0300

    feat: adiciona a entidade User na aplicacao

---
+++ src/main/java/com/learnflix/controller/response/LoginResponse.java
@@ -0,0 +1,4 @@
+package com.learnflix.controller.response;
+
+public record LoginResponse(String token) {
+}
---
+++ src/main/java/com/learnflix/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.learnflix.repository;
+
+import com.learnflix.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<UserDetails> findUserByEmail(String email);
+
+}
